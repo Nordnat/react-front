84cec7e8671295b8952cbb6a6c283f4c639bd150
Nordnat <rkamieniarz@gmail.com>
2017-08-23 21:22:12 +0200

    done

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import events from './data/events.json';
 import Events from './Events';
 
-ReactDOM.render(<Events events={events} />, document.getElementById('root'));
+ReactDOM.render(<Events />, document.getElementById('root'));
